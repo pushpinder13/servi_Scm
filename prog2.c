78ea581efd756bc67b3801f5a7ea6e82d6ee2120
Pushpinder singh <118421690+pushpinder13@users.noreply.github.com>
2023-03-14 19:35:17 +0530

    Update prog2.c

--- prog2.c
+++ prog2.c
@@ -1,1 +1,18 @@
 
+#include <stdio.h>
+int main()
+{
+
+    int a[5]={1,124,7,5,87};
+    int temp=a[4];
+    for (int i = 4; i < 0; i++)
+    {
+       a[i]=a[i-1];
+    }
+    a[0]=temp;
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ",a[i]);
+    }
+    
+}
